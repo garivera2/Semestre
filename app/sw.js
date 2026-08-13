531cd9ae1029ba92146d18776909c98233cd0a02
gscky <gabrielrivera2758@gmail.com>
2026-08-12 23:29:55 -0400

    v14: pendientes con hora y recordatorio al calendario del celular, gastos, checklist

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 // Service worker mínimo: cachea la app para que funcione sin internet.
-const CACHE = 'semestre-v13';
+const CACHE = 'semestre-v14';
 const FILES = ['./', './index.html', './guias.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
