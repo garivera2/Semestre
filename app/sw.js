b91440401a8511b25c1af90a751eea50731e375f
gscky <gabrielrivera2758@gmail.com>
2026-08-12 23:22:29 -0400

    v13: pendientes con fecha, registro de gastos con integracion Apple Pay, ajustes reorganizados

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 // Service worker mínimo: cachea la app para que funcione sin internet.
-const CACHE = 'semestre-v12';
+const CACHE = 'semestre-v13';
 const FILES = ['./', './index.html', './guias.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
